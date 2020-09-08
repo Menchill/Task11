71136c00779ec514ec2122e125f78fdf13a0c26b
Андрей Коваль <akoval@edu.hse.ru>
2020-09-08 18:39:32 +0300

    Add project files.

---
+++ Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c;
+            a = Console.ReadLine();
+            b = Console.ReadLine();
+            c = Console.ReadLine();
+            Console.WriteLine("-" + a + "-" + '\n' + "-" + b + "-" + '\n' + "-" + c + "-" + '\n');
+        }
+    }
+}
